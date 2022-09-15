375a7e50acbeb831163a7be6abeacc8ddeea3b92
Gabriel Batistuta <batistutag190@gmail.com>
2022-09-15 15:59:24 -0300

    aula 2 concluida - comentarios, variaveis com mesmo valor, aninhamento de funções em Python

---
+++ aula02/aula02.py
@@ -0,0 +1,31 @@
+# fim de função:     ;  or  another line
+
+canal = 'CFB Cursos'; curso = 'Curso de Python'
+
+canal = ' do Youtube'
+curso = 'Curso de Python'
+
+# concatenação
+
+print(curso+canal)
+
+# comentario simples
+
+"""
+
+comentario de multiplas linhas
+
+"""
+
+# varias variaveis com o mesmo valor 
+
+num1=num2=res=0
+
+print(num1); print(num2); print(res)
+
+# aninhamento de condicionais com recuo do conteudo
+
+if 2 > 2:
+ print("Maior")
+ print("aula 2")
+print("FIM")
